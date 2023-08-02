1e500e3e2cf9bfb894f9a53f3418c67f99b766d4
viraj kumbhar <virajkumbhar34@gmail.com>
2023-08-02 15:20:48 +0530

    removing generate csv menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ export const Navbar = (props) => (
       <section>
          <Link to="/">Home</Link>
          {props.isAuth && <>
-            <Link to="/createpost">Create Post</Link>
-            <Link to="/myPosts">My Post</Link>
+            <Link exact to="/createpost" className='active'>Create Post</Link>
+            <Link exact to="/myPosts" className={({isActive}) => (isActive ? "active-style" : 'none')}>My Post</Link>
          </>}
-         {!props.isAuth ? <Link to="/login" className="button blue">Login</Link> :
+         {!props.isAuth ? <Link exact to="/login" className="button blue" >Login</Link> :
             <button className='button logout' onClick={props.signUserOut}>Sign Out</button>}
       </section>
    </nav>
