fc7bc6b28c71c91f8256dd335b7536cac55ebd82
Viraj Kumbhar <viraj.10722942@ltimindtree.com>
2025-04-01 11:07:08 +0530

    Spring boot API integration

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,26 +1,42 @@
-export const GET_POSTS = 'GET POSTS'
-export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
-export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE'
+import axios from 'axios';
 
-export const getPosts = () => ({ type: GET_POSTS })
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE';
+
+export const getPosts = () => ({ type: GET_POSTS });
 export const getPostsSuccess = posts => ({
   type: GET_POSTS_SUCCESS,
   payload: posts,
-})
-export const getPostsFailure = () => ({ type: GET_POSTS_FAILURE })
+});
+export const getPostsFailure = () => ({ type: GET_POSTS_FAILURE });
 
 export function fetchPosts() {
+  const token = localStorage.getItem("token");
+  console.log("token received");
+
   return async dispatch => {
-    dispatch(getPosts())
+    dispatch(getPosts());
+
+    // try {
+    //   // Fetching posts from jsonplaceholder
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    //   const data = await response.json();
+    //   dispatch(getPostsSuccess(data));
+    // } catch (error) {
+    //   dispatch(getPostsFailure());
+    // }
 
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const data = await response.json()
-
-      dispatch(getPostsSuccess(data))
+      // Fetching posts from localhost with axios
+      const { data } = await axios.get("http://localhost:8080/blog-posts", {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      dispatch(getPostsSuccess(data));
+      console.log(data);
     } catch (error) {
-      dispatch(getPostsFailure())
+      dispatch(getPostsFailure());
+      alert("Error happened");
     }
-  }
+  };
 }
-
